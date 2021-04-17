1d747a9b747eff4a60fd63743d0174ca82ef1a00
Amina Khakimova <hakami1024@gmail.com>
2021-04-17 06:48:27 +0200

    amina: sequential training

--- training_script_amina.py
+++ training_script_amina.py
@@ -30,9 +30,9 @@ def get_data(file_id: str) -> pd.DataFrame:
 
 
 def dates_from_strings(df: pd.DataFrame) -> None:
-    df['MMM-YY'] = pd.to_datetime(df['MMM-YY'], format='%Y-%m-%d')
-    df['Dateofjoining'] = pd.to_datetime(df['Dateofjoining'], format='%Y-%m-%d')
-    df['LastWorkingDate'] = pd.to_datetime(df['LastWorkingDate'], format='%Y-%m-%d')
+    df.loc[:, 'MMM-YY'] = pd.to_datetime(df['MMM-YY'], format='%Y-%m-%d')
+    df.loc[:, 'Dateofjoining'] = pd.to_datetime(df['Dateofjoining'], format='%Y-%m-%d')
+    df.loc[:, 'LastWorkingDate'] = pd.to_datetime(df['LastWorkingDate'], format='%Y-%m-%d')
 
 
 def split_train_test_emps(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
@@ -58,7 +58,7 @@ def generate_emp_samples(df):
         employee_features[id]['Total Business Value All'] = employee_df['Total Business Value'].sum()
         employee_features[id]['Overvalue'] = (employee_df['Total Business Value'] / employee_df['Salary']).mean()
 
-        last_day = employee_df.tail(1)['LastWorkingDate'].iloc[-1]
+        last_day = pd.Timestamp(employee_df.tail(1)['LastWorkingDate'].iloc[-1])
         if pd.isnull(last_day):
             last_day = float('NaN')
             last_working_day = max_date
@@ -78,60 +78,43 @@ def generate_emp_samples(df):
     return pd.DataFrame.from_dict(employee_features, orient='index')
 
 
-def get_fitted_models(X_train, X_test, y_train, y_test):
-    pipelines = {
-        'l1': make_pipeline(StandardScaler(),
-                            LogisticRegression(penalty='l1', random_state=123, solver='liblinear')),
-        'l2': make_pipeline(StandardScaler(),
-                            LogisticRegression(penalty='l2', random_state=123)),
-        'rf': make_pipeline(
-            RandomForestClassifier(random_state=123)),
-        'gb': make_pipeline(
-            GradientBoostingClassifier(random_state=123))
-    }
+def filter_data(df, end_date):
+    df = df[(df['MMM-YY'] <= end_date) & (df['Dateofjoining'] < end_date)].copy()
+    df['LastWorkingDate'] = np.where(df['LastWorkingDate'] >= end_date, pd.NaT, df['LastWorkingDate'])
+    return df
 
-    l1_hyperparameters = {'logisticregression__C': [0.001, 0.005, 0.01,
-                                                    0.05, 0.1, 0.5, 1, 5, 10, 50, 100, 500, 1000]
-                          }
-    l2_hyperparameters = {'logisticregression__C':
-                              [0.001, 0.005, 0.01, 0.05, 0.1, 0.5,
-                               1, 5, 10, 50, 100, 500, 1000]
-                          }
 
-    rf_hyperparameters = {
-        'randomforestclassifier__n_estimators': [100, 200],
-        'randomforestclassifier__max_features': ['auto', 'sqrt', 0.33],
-        'randomforestclassifier__min_samples_leaf': [1, 3, 5, 10]
-    }
+def main():
+    with timing('Loading data'):
+        full_df = get_data(train_spreadsheet_id)
 
-    gb_hyperparameters = {
-        'gradientboostingclassifier__n_estimators': [100, 200],
-        'gradientboostingclassifier__learning_rate': [0.05, 0.1, 0.2],
-        'gradientboostingclassifier__max_depth': [1, 3, 5]
-    }
+    with timing('Processing data'):
+        dates_from_strings(full_df)
+        emp_full_df = generate_emp_samples(full_df)
 
-    hyperparameters = {
-        'l1': l1_hyperparameters,
-        'l2': l2_hyperparameters,
-        'rf': rf_hyperparameters,
-        'gb': gb_hyperparameters
-    }
+    with timing('Dividing to train and test datasets'):
+        _, test_df = split_train_test_emps(emp_full_df)
 
-    fitted_models = {}
-    for name, pipeline in pipelines.items():
-        model = GridSearchCV(pipeline,
-                             hyperparameters[name],
-                             cv=10,
-                             n_jobs=-1)
-        model.fit(X_train, y_train)
-        fitted_models[name] = model
+    full_train_data, _ = split_train_test_emps(full_df)
+    dates_from_strings(full_train_data)
 
-    for name, model in fitted_models.items():
-        print(name, model.best_score_)
-        print('Results for:', name)
+    dates = sorted(full_train_data['MMM-YY'].unique())
 
-        # obtain predictions
-        pred = fitted_models[name].predict(X_test)
+    clf = RandomForestClassifier(warm_start=True, n_estimators=90)
+
+    for date in dates[9:]:
+        with timing(f'Adding training data from the past (till {date})'):
+            data_before = filter_data(full_train_data, date)
+            emp_df = generate_emp_samples(data_before)
+            y = emp_df['Fired']
+            X = emp_df.drop('Fired', axis=1)
+            X = emp_df.drop('Emp_ID', axis=1)
+
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234,
+                                                                stratify=y)
+            clf.fit(X_train, y_train)
+
+        pred = clf.predict(X_test)
         # confusion matrix
         cm = confusion_matrix(y_test, pred)
         print(cm)
@@ -147,53 +130,21 @@ def get_fitted_models(X_train, X_test, y_train, y_test):
         print('Recall:', recall)
 
         # F1_score
-        print('F1:', f1_score(y_test, [True, ] * len(y_test)))
+        print('F1:', f1_score(y_test, pred))
 
         # obtain prediction probabilities
-        pred = fitted_models[name].predict_proba(X_test)
+        pred = clf.predict_proba(X_test)
         pred = [p[1] for p in pred]
-        # plot ROC
-        fpr, tpr, thresholds = roc_curve(y_test, pred)
-        plt.title('Receiver Operating Characteristic (ROC)')
-        plt.plot(fpr, tpr, label=name)
-        plt.legend(loc='lower right')
-        plt.plot([0, 1], [0, 1], 'k--')
-        plt.xlim([-0.1, 1.1])
-        plt.ylim([-0.1, 1.1])
-        plt.ylabel('True Positive Rate (TPR) i.e. Recall')
-        plt.xlabel('False Positive Rate (FPR)')
-        plt.show()
-
         # AUROC score
         print('AUROC:', roc_auc_score(y_test, pred))
-    return fitted_models
 
+        clf.n_estimators += 20
 
-def main():
-    with timing('Loading data'):
-        full_df = get_data(train_spreadsheet_id)
-
-    with timing('Processing data'):
-        dates_from_strings(full_df)
-        emp_full_df = generate_emp_samples(full_df)
-
-    with timing('Dividing to train and test datasets'):
-        train_df, test_df = split_train_test_emps(emp_full_df)
-
-    y = train_df['Fired']
-    X = train_df.drop('Fired', axis=1)
-
-    print('Distribution of target feature in training dataset:')
-    print(y.value_counts())
-
-    with timing('Training'):
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234,
-                                                            stratify=y)
-        fitted_models = get_fitted_models(X_train, X_test, y_train, y_test)
-        name = 'rf'
+        print('Distribution of target feature in training dataset:')
+        print(emp_df['Fired'].value_counts())
 
     with timing('Saving results'):
-        final = fitted_models[name].predict(test_df.drop('Fired', axis=1))
+        final = clf.predict(test_df.drop('Fired', axis=1))
         df_test = test_df[['Emp_ID']].copy()
         df_test['Target'] = final.astype(int)
         df_test.to_csv('output.csv', index=False)
